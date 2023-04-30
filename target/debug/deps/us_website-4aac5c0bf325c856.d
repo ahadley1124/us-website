34d44bb11971d5bbf6f8226c5725a60ae881c041
Austin Hadley <admin@austinh.dev>
2023-04-30 18:27:02 -0400

    Add to Github
    Signed-off-by: Austin Hadley <admin@austinh.dev>

---
+++ target/debug/deps/us_website-4aac5c0bf325c856.d
@@ -0,0 +1,5 @@
+/home/pi/us-website/target/debug/deps/us_website-4aac5c0bf325c856: src/main.rs
+
+/home/pi/us-website/target/debug/deps/us_website-4aac5c0bf325c856.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/us-website.d
@@ -0,0 +1,1 @@
+/home/pi/us-website/target/debug/us-website: /home/pi/us-website/src/main.rs
